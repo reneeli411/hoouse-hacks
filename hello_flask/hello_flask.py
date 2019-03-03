8364286ff2846bf3fc71e0511295e7ffbd2b54e9
srkr <srkr@umich.edu>
2019-03-03 09:49:03 -0500

    DDAY IS COMING

---
+++ hello_flask/hello_flask.py
@@ -0,0 +1,20 @@
+from flask import Flask, render_template, request
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+   return render_template('index.html')
+
+@app.route('/result',methods = ['POST', 'GET'])
+def result():
+   if request.method == 'POST':
+      result = request.form
+      age = result.get('age')
+      fam = result.get('type')
+      size = result.get('fam')
+      bedrooms = result.get('bed')
+      lang = result.get('lang')
+      return render_template("result.html",result = result)
+
+if __name__ == '__main__':
+   app.run(debug = True)
